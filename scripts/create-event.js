e505e27a15360cdb47b0890876184881183b0435
Karabo Mogano <karabomogano27@gmail.com>
2025-05-08 01:34:12 +0200

    Removed event script file to do it again via pull request

--- scripts/create-event.js
+++
@@ -1,9 +0,0 @@
-const eventForm = document.getElementById('eventForm');
-
-function getEventsFromStorage() {
-  return JSON.parse(localStorage.getItem('events')) || [];
-}
-
-function saveEventsToStorage(events) {
-  localStorage.setItem('events', JSON.stringify(events));
-}
