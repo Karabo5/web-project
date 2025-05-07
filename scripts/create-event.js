dbdf4f78324117401b9dfa70a45d23b1044ed425
Karabo Mogano <karabomogano27@gmail.com>
2025-05-07 22:40:51 +0200

    Added a create-event.js for the save button

---
+++ scripts/create-event.js
@@ -0,0 +1,9 @@
+const eventForm = document.getElementById('eventForm');
+
+function getEventsFromStorage() {
+  return JSON.parse(localStorage.getItem('events')) || [];
+}
+
+function saveEventsToStorage(events) {
+  localStorage.setItem('events', JSON.stringify(events));
+}
